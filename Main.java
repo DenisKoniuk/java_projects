1531a1c3ab2748d2503765d6b2b956fa769456db
Denis Koniuk <114607813+DenisKoniuk@users.noreply.github.com>
2022-11-22 13:18:45 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Where should russian warship go? :)");
+        var where = scanner.nextLine();
+
+        System.out.println("russian warship go " + where);
+
+    }
+}
